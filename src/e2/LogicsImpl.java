37a0018527637309518d2b269da32c7934f7bd44
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-03-09 20:13:08 +0100

    Generating random mines positions

--- src/e2/LogicsImpl.java
+++ src/e2/LogicsImpl.java
@@ -1,23 +1,35 @@
 package e2;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Random;
 
 public class LogicsImpl implements Logics {
 
     private final List<Pair<Integer, Integer>> minesList;
 
+    private final Random random;
+
+    private final int gridSize;
+
     public LogicsImpl(int size, int minesNumber) {
+        this.gridSize = size;
+        this.random = new Random();
         this.minesList = new ArrayList<>();
         for(int i = 0; i < minesNumber; i++) {
-            this.minesList.add(new Pair<>(i, i));
+            this.minesList.add(this.generateRandomPositon());
         }
     }
 
     @Override
     public List<Pair<Integer, Integer>> getMines() {
         return this.minesList.stream().toList();
+    }
+
+    private Pair<Integer,Integer> generateRandomPositon() {
+        int x = this.random.nextInt(this.gridSize);
+        int y = this.random.nextInt(this.gridSize);
+        Pair<Integer, Integer> position = new Pair<>(x, y);
+        return this.minesList.contains(position) ? this.generateRandomPositon() : position;
     }
 }
--- test/e2/LogicsTest.java
+++ test/e2/LogicsTest.java
@@ -3,6 +3,9 @@ package e2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 class LogicsTest {
 
@@ -19,5 +22,14 @@ class LogicsTest {
     @Test
     public void testNumberOfMines() {
         assertEquals(DEFAULT_MINES, this.logicImpl.getMines().size());
+    }
+
+    @Test
+    public void testDifferentMinesPositon() {
+        for (Pair<Integer,Integer> mine: this.logicImpl.getMines()) {
+           final List<Pair<Integer, Integer>> minesCopy =  new ArrayList<>(this.logicImpl.getMines());
+           assertTrue(minesCopy.remove(mine));
+           assertTrue(minesCopy.stream().noneMatch(p -> p.equals(mine)));
+        }
     }
 }
